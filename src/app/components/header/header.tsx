85706735b056a3cb11e07a6891cd931b9f983d95
Thiago Thomas <thiago_rocha17@hotmail.com>
2023-10-19 15:00:35 +0100

    feat: update button style

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,21 +1,21 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import "./header.scss"
+import "./header.scss";
 
 interface HeaderProps {
-  name: string,
-  role: string,
-  imgsrc: string
+  name: string;
+  role: string;
+  imgsrc: string;
 }
 
-export function Header({name, role, imgsrc}: HeaderProps){
-    return(
-        <div className="header">
-          <div>
-            <h1>Hi, i´m {name || "Your Name"}! 👋</h1>
-            <h2>{role || "Software Developer"}</h2>
-          </div>
-          <img src={imgsrc || "https://picsum.photos/200"}></img>
-        </div>
-    )
+export function Header({ name, role, imgsrc }: HeaderProps) {
+  return (
+    <div className="header">
+      <div>
+        <h1>Hi, i´m {name || "Your Name"}! 👋</h1>
+        <h2>{role || "Software Developer"}</h2>
+      </div>
+      <img src={imgsrc || "https://picsum.photos/200"}></img>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-import Image from 'next/image'
-import { Experience } from './components/experience/experience'
-import { Header } from './components/header/header'
-import { Info } from './components/information/information'
+import Image from "next/image";
+import { Experience } from "./components/experience/experience";
+import { Header } from "./components/header/header";
+import { Info } from "./components/information/information";
 
-import "./styles/home.scss"
-import { EmailIcon } from './components/icons/email-icon'
-import { SocialBtns } from './components/social-btns/social-btns'
+import "./styles/home.scss";
+import { EmailIcon } from "./components/icons/email-icon";
+import { SocialBtns } from "./components/social-btns/social-btns";
 
 export default function Home() {
   return (
     <main className="container">
-        <Header name="Thiago Thomas" role="Front-End Developer" imgsrc=""/>
-        <Experience/>
-        <Info/>
-        <div className="buttons">
-          <SocialBtns/>
-          <a className="btn-primary" href="mailto:teste@gmail.com">
-            contact me
-            <EmailIcon/>
-          </a>
-        </div>
+      <Header name="Thiago Thomas" role="Front-End Developer" imgsrc="" />
+      <Experience />
+      <Info />
+      <div className="buttons">
+        <SocialBtns />
+        <a className="btn-primary" href="mailto:teste@gmail.com">
+          contact me
+          <EmailIcon />
+        </a>
+      </div>
     </main>
-  )
+  );
 }
